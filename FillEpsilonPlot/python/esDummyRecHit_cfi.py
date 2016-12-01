c9b5f308144406774407ed0077baa5c63dbce8aa
crovelli <crovelli@gmail.com>
2016-12-01 17:42:38 +0100

    a few removals

--- FillEpsilonPlot/python/esDummyRecHit_cfi.py
+++
@@ -1,7 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-# dummy ES rechit producer
-ecalPreshowerRecHit = cms.EDProducer("ESDummyRecHitProducer",
-    ESRecHitCollection = cms.InputTag("ecalPreshowerRecHit","EcalRecHitsES"),
-    ESDummyRecHitCollection = cms.string('EcalRecHitsES')
-)
