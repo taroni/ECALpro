88df98a4fa03c4122803a236f26a254dc4a5088e
Silvia <Silvia.Taroni@cern.ch>
2017-09-28 10:17:25 +0200

    fixing \n

--- submit/methodsZFromRaw.py
+++ submit/methodsZFromRaw.py
@@ -208,7 +208,7 @@ def printSubmitSrc(outputfile, cfgName, source, destination, pwd, logpath):
     outputfile.write("#!/bin/bash\n")
     outputfile.write("cd " + pwd + "\n")
     outputfile.write("eval `scramv1 runtime -sh`\n")
-    outputfile.write("export X509_USER_PROXY=/afs/cern.ch/user/t/taroni/x509up_u29820")
+    outputfile.write("export X509_USER_PROXY=/afs/cern.ch/user/t/taroni/x509up_u29820 \n")
     if not(Silent):
         outputfile.write("echo 'cmsRun " + cfgName + "'\n")
         outputfile.write("cmsRun " + cfgName + "\n")
